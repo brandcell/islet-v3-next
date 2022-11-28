588c1343146ec1c385eefcdf33caa2d6744d588b
purdator1225 <briansh1225@gmail.com>
2022-11-28 23:08:06 +0800

    added navbar selection indicator

--- components/layouts/category-layout.component.js
+++ components/layouts/category-layout.component.js
@@ -1,50 +1,87 @@
-
-import styled from 'styled-components'
-import Link from 'next/link';
-import { AnimatePresence, motion } from 'framer-motion';
-import Navbar from '../Navbar/navBar';
-
+import styled from "styled-components";
+import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
+import Navbar from "../Navbar/navBar";
+import { useRouter } from "next/router";
 
 const StyledLinkContainer = styled.div`
   max-width: 657px;
   min-height: 38px;
   margin: auto;
-  margin-bottom:25px;
+  margin-bottom: 25px;
   display: flex;
   justify-content: space-evenly;
   flex-wrap: wrap;
 
-  & a{
+  & p {
     font-family: Signifier;
     font-size: 20px;
-    color:white;
+    color: white;
   }
 `;
 
+function CategoryLayout({ children }) {
 
+  const router = useRouter();
 
-function CategoryLayout({children}) {
+  console.log(router.pathname)
+
   return (
     <>
-      <Navbar color='white'/>
+      <Navbar color="white" />
 
-<div className='portfolio-page-wrapper'>
- <StyledLinkContainer>
-    <Link href="/portfolios">All</Link>
-    <Link href="/portfolios/category/product">Product</Link>
-    <Link href="/portfolios/category/commercials">Commercial</Link>
-    <Link href="/portfolios/category/documentaries">Documentaries</Link>
-    <Link href="/portfolios/category/corporate">Corporate</Link>
-  </StyledLinkContainer>
+      <div className="portfolio-page-wrapper">
+        <StyledLinkContainer>
+          <Link
+            className={router.asPath === "/portfolios" ? "active" : ""}
+            href="/portfolios"
+          >
+            <p>All</p>
+          </Link>
+          <Link
+            className={
+              router.asPath === "/portfolios/category/product" ? "active" : ""
+            }
+            href="/portfolios/category/product"
+          >
+            <p>Product</p>
+          </Link>
+          <Link
+            className={
+              router.asPath === "/portfolios/category/commercials"
+                ? "active"
+                : ""
+            }
+            href="/portfolios/category/commercials"
+          >
+            <p>Commercial</p>
+          </Link>
+          <Link
+            className={
+              router.asPath === "/portfolios/category/documentaries"
+                ? "active"
+                : ""
+            }
+            href="/portfolios/category/documentaries"
+          >
+            <p>Documentaries</p>
+          </Link>
+          <Link
+            className={
+              router.asPath == "/portfolios/category/corporate"
+                ? "active"
+                : ""
+            }
+            href="/portfolios/category/corporate"
+          >
+            <p>Corporate</p>
+          </Link>
+        </StyledLinkContainer>
 
-<AnimatePresence>
-{children}
-</AnimatePresence>
-  
-</div>
+        <AnimatePresence>{children}</AnimatePresence>
+      </div>
     </>
-    
-  )
+  );
 }
 
-export default CategoryLayout
+export default CategoryLayout;
