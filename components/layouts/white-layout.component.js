7eb8527fb28e40d90bada6c98190a00ad4cd73b2
purdator1225 <briansh1225@gmail.com>
2022-11-30 17:20:59 +0800

    put bg in nav

--- components/layouts/white-layout.component.js
+++ components/layouts/white-layout.component.js
@@ -4,8 +4,9 @@ import Footer from '../footer/footer.component'
 function WhiteLayout({children}) {
   return (
     <>
-
+ <div style={{background:'white', position:'fixed', top:'0px',height:'100px', width:'100vw', zIndex:'80'}}></div>
         <Navbar color='black'/>
+       
 
         {children}
         
