e79e4d55ef7316d1ced6ab884a0af7927a1e4b3a
purdator1225 <briansh1225@gmail.com>
2022-12-06 13:56:06 +0800

    changed founders para

--- components/founder-component/founder.component.js
+++ components/founder-component/founder.component.js
@@ -5,9 +5,9 @@ import robertUrl from '../../public/founders-pics/robert.png'
 import soogsUrl from '../../public/founders-pics/soogs.png'
 
 
-const robertPara = 'We believe joy is the engine that powers all the best ideas. So we’re designing a way of working that makes. We believe joy is the engine that powers all the best ideas. So we’re designing a way of working that makes.'
+const robertPara = 'As a multi-talented filmmaker, Robert easily code-switches between various roles. Besides being the Creative Lead and Co-founder of Islet Studio, Robert is a Short Documentary Director, Tabletop Director, and Cinematographer who has worked for the likes of Grab, DC Asia, Tik Tok, Bumble, Certis, and Telstra. Drawn to filmmaking for its way of emotional engagement like no other, Robert isn’t afraid of breaking traditional storytelling conventions to create impactful and cherished films. When he is not obsessing about filmmaking, he enjoys longboarding, and attending to his exotic plants and beloved feline, Chiri.'
 
-const soogsPara = 'We believe simple is magic, and business software should be elegant and intuitive—your tools for work should work for you. We believe joy is the engine that powers all the best ideas. So we’re designing a way of working that makes. We believe you should work the way you want. One person’s perfect workflow is another’s formula for burnout.'
+const soogsPara = 'Soo Ghee is the Co-founder of Islet Studio, Director, and Sound Engineer with 4 years of experience in the video production industry. Calm, collected, and with an eye for the bigger picture, Soo Ghee can always be counted on to make crucial decisions on set. He derives great pleasure from collaborative filmmaking, even the grueling hours it takes to bring a film to life. To him, it’s all worth it when the final cut is exactly as envisioned. In his downtime, Soo Ghee likes to catch up on the latest NBA highlights and is quite the wine enthusiast.'
 
 function FounderSection() {
   return (
