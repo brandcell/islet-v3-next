e0bb47f5013724e7e20393c8ef6f31873af90df0
purdator1225 <briansh1225@gmail.com>
2023-01-03 21:21:36 +0800

    set up gtm

--- pages/_app.js
+++ pages/_app.js
@@ -5,42 +5,35 @@ import { motion, AnimatePresence } from "framer-motion";
 import Router, { useRouter } from "next/router";
 import Script from "next/script";
 
-
-
-
 function MyApp({ Component, pageProps }) {
-
   const Layout = Component.Layout || EmptyLayout;
- const router = useRouter
+  const router = useRouter;
 
   return (
     <>
-      <Script strategy="lazyOnLoad" src={`https://www.googletagmanager.com/gtag/js?id=G-L89JE8VWSY`}/>
+      <Script id="gtm-script"
+        strategy="lazyOnLoad"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-L89JE8VWSY`}
+      />
 
-      <Script strategy="lazyOnLoad">{` window.dataLayer = window.dataLayer || [];
+      <Script id="datalayer-script" strategy="lazyOnLoad">{` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-L89JE8VWSY');`}</Script>
       <NavProvider>
-      
         <Layout>
-        <AnimatePresence>
-       
-        
-          <Component {...pageProps} key={router.asPath}/>
+          <AnimatePresence>
+            <Component {...pageProps} key={router.asPath} />
           </AnimatePresence>
-          
         </Layout>
-        
-      
       </NavProvider>
-      
     </>
-    
   );
 }
 
-const EmptyLayout = ({ children }) => <AnimatePresence exitBeforeEnter>{children}</AnimatePresence>;
+const EmptyLayout = ({ children }) => (
+  <AnimatePresence exitBeforeEnter>{children}</AnimatePresence>
+);
 
 export default MyApp;
