1ab6eed2be3df5021b29a8d2160965e02cc0e969
purdator1225 <briansh1225@gmail.com>
2023-01-03 22:22:46 +0800

    added gtm

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,35 @@ import { NavProvider } from "../contexts/navbar.context";
 import { motion, AnimatePresence } from "framer-motion";
 import Router, { useRouter } from "next/router";
 import Script from "next/script";
+import TagManager, {TagManagerArgs} from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || EmptyLayout;
   const router = useRouter;
 
+  //google tag manager code
+  const gtmId = 'GTM-KMG87VF'
+
+  const TagManagerArgs = {gtmId}
+
+  useEffect(() => {
+    TagManager.initialize(TagManagerArgs)
+   
+  }, []);
+
   return (
     <>
-      <Script id="gtm-script"
+      <Script
+        id="gtm-script"
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-L89JE8VWSY`}
       />
 
-      <Script id="datalayer-script" strategy="afterInteractive">{`window.dataLayer = window.dataLayer || [];
+      <Script
+        id="datalayer-script"
+        strategy="afterInteractive"
+      >{`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
