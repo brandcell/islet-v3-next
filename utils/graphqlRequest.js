378aa0661173e69c649a04469a2d7d67c84ad9a5
brian <briansh1225@gmail.com>
2024-05-15 00:29:30 +0800

    final

--- utils/graphqlRequest.js
+++ utils/graphqlRequest.js
@@ -3,6 +3,8 @@ import { API_URL } from "./urls";
 export default async function graphQlRequest(query) {
   const url = `${API_URL}/graphql`;
 
+  console.log(url);
+
   const headers = { "Content-Type": "application/json" };
 
   const res = await fetch(url, {
--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,5 @@
 export const API_URL =
-  "https://islet-strapi-v3-f29e3a83c355" || "http://localhost:1337";
+  "https://islet-strapi-v3-f29e3a83c355.herokuapp.com" ||
+  "http://localhost:1337";
 
 // process.env.NEXT_PUBLIC_API_URL
