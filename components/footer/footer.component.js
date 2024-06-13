31958b5fddc385842224e122a70ebeccd22de666
brian <briansh1225@gmail.com>
2024-06-13 17:57:42 +0800

    changed footer

--- components/footer/footer.component.js
+++ components/footer/footer.component.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import Link from "next/link";
 
-
-const FooterWrapper= styled.div`
-background-color:#43594d;
-width:100vw`
+const FooterWrapper = styled.div`
+  background-color: #43594d;
+  width: 100vw;
+`;
 
 const FooterContainer = styled.div`
   display: flex;
@@ -39,12 +39,11 @@ const FooterTopContainer = styled.div`
   justify-content: space-between;
   flex-wrap: wrap;
 
-  & :nth-child(3){
-    :hover{
+  & :nth-child(3) {
+    :hover {
       color: white;
       cursor: pointer;
     }
-    
   }
 `;
 
@@ -101,53 +100,64 @@ const ContactInfoContainer = styled.div`
 function Footer() {
   return (
     <FooterWrapper>
-    <FooterContainer>
-      <FooterTopContainer>
-        <FooterInnerContainers>
-          <h1>Don’t be shy, let’s chat!</h1>
-        </FooterInnerContainers>
-        <FooterInnerContainers>
-          <ContactInfoContainer>
+      <FooterContainer>
+        <FooterTopContainer>
+          <FooterInnerContainers>
+            <h1>Don’t be shy, let’s chat!</h1>
+          </FooterInnerContainers>
+          <FooterInnerContainers>
+            <ContactInfoContainer>
+              <a
+                target="_blank"
+                href="https://www.google.com/maps/place/Lengkok+York,+Taman+Dhoby+Ghaut,+10450+George+Town,+Pulau+Pinang/@5.4138573,100.3054329,17z/data=!3m1!4b1!4m5!3m4!1s0x304ac3b4f7ca62f5:0xf2126c9487c7e9d5!8m2!3d5.4138573!4d100.3076216"
+                rel="noopener noreferrer"
+              >
+                <h4>Islet Studio</h4>
+                <h4>
+                  36 Lengkok York, Taman Dhoby Ghaut, 10450 Georgetown Pulau
+                  Pinang
+                </h4>
+              </a>
+            </ContactInfoContainer>
 
-          <a target="_blank" href="https://www.google.com/maps/place/Lengkok+York,+Taman+Dhoby+Ghaut,+10450+George+Town,+Pulau+Pinang/@5.4138573,100.3054329,17z/data=!3m1!4b1!4m5!3m4!1s0x304ac3b4f7ca62f5:0xf2126c9487c7e9d5!8m2!3d5.4138573!4d100.3076216" rel="noopener noreferrer">
-          <h4>Islet Studio</h4>
-              <h4>36 Lengkok York, Taman Dhoby Ghaut, 10450 Georgetown Pulau Pinang</h4>
-            </a>
+            <ContactInfoContainer>
+              <a
+                className="email-click"
+                target="_blank"
+                href="mailto:hello@isletstudio.com?subject=Hello!%20"
+                rel="noopener noreferrer"
+              >
+                <h4>hello@isletstudio.com</h4>
+              </a>
+            </ContactInfoContainer>
+            <ContactInfoContainer>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/robertvisuals.mov/?hl=en"
+                rel="noopener noreferrer"
+              >
+                <h4>Instagram</h4>
+              </a>
 
-
-          </ContactInfoContainer>
-
-          <ContactInfoContainer>
-         
-
-            <a className="email-click" target="_blank" href="mailto:hello@isletstudio.com?subject=Hello!%20" rel="noopener noreferrer">
-          <h4>hello@isletstudio.com</h4>
-            </a>
-  
-          </ContactInfoContainer>
-          <ContactInfoContainer>
-
-          <a target="_blank" href='https://www.instagram.com/robertvisuals.mov/?hl=en' rel="noopener noreferrer">
-          <h4>Instagram</h4>
-            </a>
-
-            <a target="_blank" href='https://www.facebook.com/RobVisuals/' rel="noopener noreferrer">
-            <h4>Facebook</h4>
-            </a>
-
-
-          </ContactInfoContainer>
-        </FooterInnerContainers>
-        <FooterInnerContainers 
-          onClick={() => {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }}
-        >
-          Back to Top
-        </FooterInnerContainers>
-      </FooterTopContainer>
-      <p>All rights reserved © Islet Studio 2023</p>
-    </FooterContainer>
+              <a
+                target="_blank"
+                href="https://www.facebook.com/RobVisuals/"
+                rel="noopener noreferrer"
+              >
+                <h4>Facebook</h4>
+              </a>
+            </ContactInfoContainer>
+          </FooterInnerContainers>
+          <FooterInnerContainers
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
+            Back to Top
+          </FooterInnerContainers>
+        </FooterTopContainer>
+        <p>All rights reserved © Islet Studio 2024</p>
+      </FooterContainer>
     </FooterWrapper>
   );
 }
